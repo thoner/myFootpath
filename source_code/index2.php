60b63aea7d0b148c14bc33b0419d997b1d3eb0a8
thoner <thoner@mail.ru>
2016-04-23 02:08:18 +0600

    Added files via upload

---
+++ source_code/index2.php
@@ -0,0 +1,137 @@
+<?php
+
+	include 'connection.php';
+	
+	$table1 = "SELECT id, status_label , count(status_label) FROM `leads` group by  status_label";
+	$table2 = "SELECT * FROM `lead_custom`";
+	
+	
+	
+	if(mysqli_query($connection, $table1) && (mysqli_query($connection, $table2)))
+	{	
+		$result1 = mysqli_query($connection, $table1);
+		$result2 = mysqli_query($connection, $table2);
+		
+		
+		$count=0;
+		$flag = false;
+		$array[0]=0;
+		
+		
+			
+		while($row1 = mysqli_fetch_array($result1)) 
+		{	
+
+		  while($row2 = mysqli_fetch_array($result2))
+		  {   
+							
+			if ($row1['id'] == $row2['lead_id'] )
+			{	
+								
+				$flag = true;
+				$count = $row1[2];
+				array_push($array, $row2['field_value'] ,  $row1['status_label'], $count);
+				
+			}
+			
+			
+		  }
+
+		mysqli_data_seek($result2, 0);
+		$count=0;
+		}
+		
+
+		if($flag == true)
+		{
+			
+			echo "<br />Asignee |	Status | Count<br />";
+		
+			$array = array_keys(array_flip($array)); //remove duplicated rows 
+			
+			for ($i=1; $i < count($array); $i++)
+			{
+				
+				if($i%3 != 0)
+				{
+					echo  $array[$i] . " | ";
+				}
+				else
+				{
+					echo $array[$i] . "<br />";
+				}
+					
+				
+			}
+			
+		}
+		else
+		{
+			$query1 = "SELECT  status_label  FROM `leads`"; 
+			if($query1 = mysqli_query($connection, $query1))
+			{
+				echo "<br />All Asignee";
+				echo "<br />";
+
+				while($row11[0] = mysqli_fetch_array($query1)) {
+				echo $row11[0][0] . "<br />";
+				}
+			
+				echo "<br />";
+				
+			}
+			
+			
+						
+			$query2 = "SELECT  field_value  FROM `lead_custom`";
+			if($query2 = mysqli_query($connection, $query2))
+			{
+				echo "<br />All Statuses";
+				echo "<br />";
+
+				while($row11[0] = mysqli_fetch_array($query2)) {
+				echo $row11[0][0] . "<br />";
+				}
+			
+				echo "<br />";
+				
+			}
+			
+		}
+				
+	}
+	else 
+	{
+			$query1 = "SELECT  status_label  FROM `leads`"; 
+			if($query1 = mysqli_query($connection, $query1))
+			{
+				echo "<br />All Asignee";
+				echo "<br />";
+
+				while($row11[0] = mysqli_fetch_array($query1)) {
+				echo $row11[0][0] . "<br />";
+				}
+			
+				echo "<br />";
+				
+			}
+			
+		
+			$query2 = "SELECT  field_value  FROM `lead_custom`";
+			if($query2 = mysqli_query($connection, $query2))
+			{
+				echo "<br />All Statuses";
+				echo "<br />";
+
+				while($row11[0] = mysqli_fetch_array($query2)) {
+				echo $row11[0][0] . "<br />";
+				}
+			
+				echo "<br />";
+				
+			}
+		
+	}
+	
+
+?>
